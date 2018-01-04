1724b2899afd991c664b5f837c2fb73fb9d21f67
[Realera95] <klovertrask95@gmail.com>
2018-01-04 07:38:54 +0100

    Added timestamp to log output

--- SMHIweatherInfo.py
+++ SMHIweatherInfo.py
@@ -1,35 +1,53 @@
 import requests
 import json
 from datetime import datetime
-import logging
+import traceback
+import sys
+
 
 class SMHI(object):
     def __init__(self,lat,lon):
         self.lat = lat
         self.lon = lon
         self.host = 'https://opendata-download-metfcst.smhi.se'
-        self.Logger = logging.basicConfig(filename='SMHI_Errorlog.txt', level=logging.ERROR)
+        self.lastUpdated = datetime.strptime('1900-01-01T00:00:00Z',\
+                                            '%Y-%m-%dT%H:%M:%SZ')
+        self.temp = 0
+        self.tempUnit = ''
         self.update()
 
 
     def update(self):
         try:
-            self.r = requests.get(self.host + \
-                                '/api/category/pmp3g/version/2/geotype/point/lon/{0}/lat/{1}/data.json'\
+            r = requests.get(self.host + \
+                                '/api/category/pmp3g/version/2/geotype/point/' + \
+                                'lon/{0}/lat/{1}/data.json'\
                                 .format(self.lat, self.lon))
 
-            if self.r.status_code != requests.codes.ok:
-                raise Exception("Status code from response not Ok.")
-
-            self.parsedJSON = json.loads(self.r.text)
-            self.lastUpdated = datetime.strptime(self.parsedJSON['approvedTime'],\
+            if r.status_code != requests.codes.ok:
+                raise Exception('Status code from response not Ok.\n' + \
+                                'HTTP code = {0}'.format(r.status_code))
+            1/0
+            parsedJSON = json.loads(r.text)
+            self.lastUpdated = datetime.strptime(parsedJSON['approvedTime'],\
                                                 '%Y-%m-%dT%H:%M:%SZ')
-            self.temp = self.parsedJSON['timeSeries'][0]['parameters'][11]['values']
-            self.tempUnit = self.parsedJSON['timeSeries'][0]['parameters'][11]['unit']
+            self.temp = parsedJSON['timeSeries'][0]['parameters'][11]['values'][0]
+            self.tempUnit = parsedJSON['timeSeries'][0]['parameters'][11]['unit']
 
         except Exception:
-            logging.exception('Message')
+            self.LogWriter(traceback.format_exc())
             exit(1)
 
+    def LogWriter(self,e):
+        fh = open('SMHI_Errorlog.txt','a')
+        s = '*-------------------*' + '\n'
+        s = s + 'Time: ' + str(datetime.now()) + '\n'
+        s = s + str(e)
+        s = s + '*-------------------*' + '\n' + '' + '\n' + '' + '\n'
+        fh.write(s)
+        fh.close()
+
 if __name__ == '__main__':
-    smhi_info = SMHI(21.4047,65)
+    smhi_info = SMHI(22.1567,65.5848)
+    print(str(smhi_info.temp) + ' ' + smhi_info.tempUnit)
+    print(smhi_info.lastUpdated)
